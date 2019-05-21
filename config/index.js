1ffde9c9047cab5ace2142dad08c53c3707c77d0
allen <kqkq5511@gmail.com>
2019-05-21 20:55:29 +0800

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vue-api',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
