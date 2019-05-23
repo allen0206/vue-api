ece03b98d7900ccb3a343ece8d4dd9b952ac118a
allen <kqkq5511@gmail.com>
2019-05-23 21:36:34 +0800

    test

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './vue-api/dist/',
+    assetsPublicPath: '/vue-api/dist/',
 
     /**
      * Source Maps
